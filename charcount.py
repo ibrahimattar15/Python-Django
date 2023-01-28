89327cf910f2190da31bc6d1f148c5dcb490d74e
ibrahimattar15 <ibrahimattar15@gmail.com>
2023-01-28 19:00:12 +0530

    code wrapped in function

--- charcount.py
+++ charcount.py
@@ -1,13 +1,17 @@
 print('Welcome in char count program....')
+def check_char_count(s):
+    n = len(s)
+    output = ''
+    count = 1
+    for i in range(n-1):
+        if s[i]==s[i+1]:#if first word and previous word are same then increase count
+            count+=1
+        else:
+            output =output+str(count)+s[i]# if previous and present word not same then count =1
+            count =1
+    output = output+str(count)+s[n-1] # count last char
+    print(output)
 s = input('Enter the string:')
-n = len(s)
-output = ''
-count = 1
-for i in range(n-1):
-    if s[i]==s[i+1]:#if first word and previous word are same then increase count
-        count+=1
-    else:
-        output =output+str(count)+s[i]# if previous and present word not same then count =1
-        count =1
-output = output+str(count)+s[n-1] # count last char
-print(output)
+check_char_count(s)
+print('end of program...')
+print('Thank you....')
