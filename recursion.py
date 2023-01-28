cbbc198f184a98f3372f7ffd8db64fa3f80c2ced
ibrahimattar15 <ibrahimattar15@gmail.com>
2023-01-28 19:41:11 +0530

    reverse string recursion function added

--- recursion.py
+++ recursion.py
@@ -20,3 +20,11 @@ for i in range(n):
     print(fibonacci(i),end=' ')
 fibonacci(n)
 
+#3 reverse string
+def reverse(s):
+    if len(s)==0:
+        return s
+    else:
+        return reverse(s[1:])+s[0]
+s = input('Enter the string:')
+print(reverse(s))
